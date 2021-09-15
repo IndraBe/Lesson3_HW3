4dd405d3b52d89125c7a2b2cae2719cd123d2f57
Indra <indraberga@inbox.lv>
2021-09-15 21:14:41 +0300

    About_classes_objects

---
+++ Lesson3_app/Lesson3_app/Person.cs
@@ -0,0 +1,40 @@
+﻿using System; //citas bibliotēkas, folderi, ko izmanto
+
+namespace Lesson3_app
+{
+    public class Person
+    {
+        public Person(string name) //konstruktors. Lietotājam obligāti jāievada vārds un to pēc tam nedrīkst mainīt. Konstruktora nosaukuma jāsakrīt ar klases nosaukumu.
+        {
+            Name = name;
+        }
+
+        public string Name { get; } //noņem set, lai nebūtu iespējams mainīt vārdu. To uzstāda klases izveides brīdī un tāds arī paliek.
+
+        //Property pilnais pieraksts. Katru reizi, kad personai tiek uzstādīts vecums, tas tiek uzstādits pozitīvs. Piem., uzstāda -30, patiesais vecums 30.
+        private int _age; //pieejams tikai klases iekšienē, Nav pieejams uz ārpusi redzēšanai. Dati privāti, bet caur publiskām funkcijām to dabū uz āru (get & set).
+        public int Age // publiski tiek izvadīts interface
+        {
+            get
+            {
+                return _age; //iegūst informāciju
+            }
+            set
+            {
+                _age = Math.Abs(value); //nosaka informāciju
+            }
+        }
+        public double Money { get; set; }
+
+        public bool IsMale { get; set; }
+
+        public char Initial { get; set; }
+
+        public string getGreeting()
+        {
+            string greeting = "Hello my name is " + Name + "and I am " + Age + " years old";
+
+            return greeting;
+        }
+    }
+}
---
+++ Lesson3_app/Lesson3_app/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson3_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Indra"); //Person - Klase. person ir Objekts. svarīgākais - NEW. Vārds pēdiņās konstruktors no klases.
+
+            Person person2 = person; //piekļūst vienam un tam pašam objektam. Jauna persona izveidojas tikai ar NEW. Referencējas tieši uz to pašu objektu.
+
+            //person.Name = "Indra"; //uzstāda datus / uz vērtību nokļūst caur .
+            person.Age = 15;
+            person.IsMale = true;
+            person.Money = 160.60;
+            person.Intial = 'I';
+
+            person2.Age = 26;  //2 mainīgie, viena vieta atmiņā, tāpēc pirmo nozīmēto vērtību aizstāj otra nozīmētā vērtība
+
+            string greeting = person.getGreeting();
+
+            Console.WriteLine(greeting);
+        }
+    }
+}
