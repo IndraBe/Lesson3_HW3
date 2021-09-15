0c424cf7b3e0141671aa49ba2908469ff44dbae8
Indra <indraberga@inbox.lv>
2021-09-15 23:11:22 +0300

    HW3_start

---
+++ HW3/HW3/Phone.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    public class Phone
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public double LengthSize { get; set; }
+
+        public double WidthSize { get; set; }
+
+        public double HeightSize { get; set; }
+
+        public string Call()
+        {
+            string getCall = "Contact us by calling";
+
+            return getCall;
+        }
+        public string sendSMS()
+        {
+            string sms = "Contact us sending SMS";
+
+            return sms;
+        }
+        
+    }
+}
---
+++ HW3/HW3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone = new Phone();
+
+            Console.WriteLine("Nosauciet telefona marku");
+
+            string phoneBrand = Console.ReadLine();
+            
+            Console.WriteLine(phoneBrand);
+            //phone.Brand = "IPhone";
+            //phone.Model = "Pro";
+            //phone.LengthSize = 15;
+            //phone.WidthSize = 8;
+            //phone.HeightSize = 2;
+
+            //string getCall = phone.Call();
+            //Console.WriteLine(getCall);
+
+            //string sms = phone.sendSMS();
+            //Console.WriteLine(sms);
+        }
+    }
+}
