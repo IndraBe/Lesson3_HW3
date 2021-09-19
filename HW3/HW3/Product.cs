98aa3014299fdcfd68f384ff5b2c0a2c107984de
Indra <indraberga@inbox.lv>
2021-09-19 13:28:20 +0300

    3rd task-all except Show method

---
+++ HW3/HW3/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class Product
+    {
+
+        public Product(double productLength, double productWidth, int productWeight)       //konstruktors
+        {
+            Length = productLength;
+            Width = productWidth;
+            Weight = productWeight;
+        }
+
+        public double Length { get; }   //field
+
+        public double Width { get; }   //field
+
+        public double Weight { get; }   //field
+
+        public string Create()                  //method
+        {
+            string createProduct = "Jauns produkts ir izveidots.";
+            return createProduct;
+        }
+
+        //public string Show()               //method
+        //{
+        //    string showProduct = "Product length" ;
+        //    return showProduct;
+        //}
+    }
+}
--- HW3/HW3/Program.cs
+++ HW3/HW3/Program.cs
@@ -6,83 +6,112 @@ namespace HW3
     {
         static void Main(string[] args)         //call a method
         {
-            Phone newObj1 = new Phone();          //creating object
+            //Phone newObj1 = new Phone();          //creating object
 
-            Console.WriteLine("Nosauciet telefona zīmolu:");
+            //Console.WriteLine("Nosauciet telefona zīmolu:");
 
-            newObj1.Brand = Console.ReadLine();
-
-
-            Console.WriteLine("Nosauciet telefona modeli:");
-
-            newObj1.Model = Console.ReadLine();
+            //newObj1.Brand = Console.ReadLine();
 
 
-            Console.WriteLine("Lūdzu, norādiet katrā rindā telefona garumu, platumu un augstumu:");
+            //Console.WriteLine("Nosauciet telefona modeli:");
 
-            newObj1.LengthSize = double.Parse(Console.ReadLine());
-            newObj1.WidthSize = double.Parse(Console.ReadLine());
-            newObj1.HeightSize = double.Parse(Console.ReadLine());
+            //newObj1.Model = Console.ReadLine();
 
-            Console.WriteLine("Jūs izvēlējāties šādu telefonu: " +
-                                newObj1.Brand + " " +
-                                newObj1.Model + " / " +
-                                newObj1.LengthSize + " / " +
-                                newObj1.WidthSize + " / " +
-                                newObj1.HeightSize);
 
-            string makeCall = newObj1.Call();
-            Console.WriteLine(makeCall);
+            //Console.WriteLine("Lūdzu, norādiet katrā rindā telefona garumu, platumu un augstumu:");
 
-            string sendSMS = newObj1.SMS();
-            Console.WriteLine(sendSMS);
+            //newObj1.LengthSize = double.Parse(Console.ReadLine());
+            //newObj1.WidthSize = double.Parse(Console.ReadLine());
+            //newObj1.HeightSize = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("================================================================================");
+            //Console.WriteLine("Jūs izvēlējāties šādu telefonu: " +
+            //                    newObj1.Brand + " " +
+            //                    newObj1.Model + " / " +
+            //                    newObj1.LengthSize + " / " +
+            //                    newObj1.WidthSize + " / " +
+            //                    newObj1.HeightSize);
 
-            Car newCar1 = new Car();          //creating object
+            //string makeCall = newObj1.Call();
+            //Console.WriteLine(makeCall);
 
-            Console.WriteLine("Nosauciet mašīnas marku, nummurzīmi un pašreizējo ātrumu:");
+            //string sendSMS = newObj1.SMS();
+            //Console.WriteLine(sendSMS);
 
-            newCar1.carBrand = Console.ReadLine();
-            newCar1.carPlate = Console.ReadLine();
-            newCar1.carSpeed = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Jūsu izvēlējātā mašīna ir ar šādiem parametriem: " +
-                                    newCar1.carBrand + " / " +
-                                    newCar1.carPlate + " / " +
-                                    newCar1.carSpeed + " km/h");
+            //Console.WriteLine("================================================================================");
 
-            Console.WriteLine("-------------------------");
+            //Car newCar1 = new Car();          //creating object
 
-            int speed = newCar1.carSpeed;
-            bool fromStart = speed == 0;
+            //Console.WriteLine("Nosauciet mašīnas marku, nummurzīmi un pašreizējo ātrumu:");
 
-            string startDriving = newCar1.Start();
-            string continueDriving = newCar1.contSpeed();
-            Console.WriteLine(fromStart ? startDriving : continueDriving);
+            //newCar1.carBrand = Console.ReadLine();
+            //newCar1.carPlate = Console.ReadLine();
+            //newCar1.carSpeed = int.Parse(Console.ReadLine());
 
-            Console.ReadLine();
+            //Console.WriteLine("Jūsu izvēlējātā mašīna ir ar šādiem parametriem: " +
+            //                        newCar1.carBrand + " / " +
+            //                        newCar1.carPlate + " / " +
+            //                        newCar1.carSpeed + " km/h");
 
-            string upSpeed = newCar1.IncreaseSpeed();
-            Console.WriteLine(upSpeed);
+            //Console.WriteLine("-------------------------");
 
-            Console.ReadLine();
+            //int speed = newCar1.carSpeed;
+            //bool fromStart = speed == 0;
 
-            string speedDown = newCar1.DecreaseSpeed();
-            Console.WriteLine(speedDown);
+            //string startDriving = newCar1.Start();
+            //string continueDriving = newCar1.contSpeed();
+            //Console.WriteLine(fromStart ? startDriving : continueDriving);
 
-            Console.ReadLine();
+            //Console.ReadLine();
 
-            string beep = newCar1.Signal();
-            Console.WriteLine(beep);
+            //string upSpeed = newCar1.IncreaseSpeed();
+            //Console.WriteLine(upSpeed);
 
-            Console.ReadLine();
+            //Console.ReadLine();
 
-            string stopDriving = newCar1.Stop();
-            Console.WriteLine(stopDriving);
+            //string speedDown = newCar1.DecreaseSpeed();
+            //Console.WriteLine(speedDown);
+
+            //Console.ReadLine();
+
+            //string beep = newCar1.Signal();
+            //Console.WriteLine(beep);
+
+            //Console.ReadLine();
+
+            //string stopDriving = newCar1.Stop();
+            //Console.WriteLine(stopDriving);
 
             //string startDriving = newCar1.Start();        //Vai viena un tā pati metode var tikt izsaukta vairākas reizes? Piemēram, atkal uzsākt braukt.
             //Console.WriteLine(startDriving);
+
+
+            Console.WriteLine("================================================================================");
+
+            Console.WriteLine("Lūdzu ievadiet preces garumu, platumu un svaru:");
+
+            double lengthsize = double.Parse(Console.ReadLine());
+
+            double widthhsize = double.Parse(Console.ReadLine());
+
+            int weight = int.Parse(Console.ReadLine());
+
+            Product newProduct1 = new Product(lengthsize, widthhsize, weight);          //creating object
+
+            Console.WriteLine("Press Enter"); 
+            Console.ReadLine();
+
+            string createProduct = newProduct1.Create();
+            Console.WriteLine(createProduct);
+
+            //string showProduct = newProduct1.Show();
+            //Console.WriteLine(showProduct);
+
+            //Console.WriteLine(newProduct1.Length);
+            //Console.WriteLine(newProduct1.Width);
+
+            //newProduct1.Length = 34.6;                //to test if not possible to change
+            //newProduct1.Width = 34.6;
         }
     }         
 }
