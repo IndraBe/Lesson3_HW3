a0ac442712600cdc2f5081aef8e33a28b2ff0e4e
Indra <indraberga@inbox.lv>
2021-09-20 09:40:46 +0300

    4th class_Person_w/o age

---
+++ HW3/HW3/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class Person
+    {
+        public Person(string name, string surname)
+        { 
+            Name = name;
+            Surname = surname;
+        }   
+        public string Name { get; }
+
+        public string Surname { get; }   //field
+
+        public string BirthDate { get; set; }  //field
+
+        public string Hobby { get; set; }  //field
+
+        public string Gender { get; set; }  //field
+
+        public string SayHi()                  //method
+        {
+            string greet = "Hello my name is " + Name + " " + Surname + ".";
+            return greet;
+        }
+    }
+}
